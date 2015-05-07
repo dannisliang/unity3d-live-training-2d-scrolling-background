ea8910c7ddf7000f0bb7b392e7d7ef4f5921c211
kieronlanning <kieron@outlook.com>
2015-05-07 06:11:08 +0100

    Parallax tutorial from Unity3D.com

---
+++ 2D Scrolling Background/Assets/Scripts/OffsetScroller.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OffsetScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+
+    Vector2 _savedOffset;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset("_MainTex");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var y = Mathf.Repeat(Time.time*scrollSpeed, 1);
+        var offset = new Vector2(_savedOffset.x, y);
+
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+
+    void OnDisable()
+    {
+        // Reset the offset to the startup value.
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", _savedOffset);
+    }
+}
---
+++ 2D Scrolling Background/Assets/Scripts/StripScroller.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StripScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSizeZ;
+
+    Vector3 startPosition;
+    Vector2 _savedOffset;
+
+    void Start()
+    {
+        _savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset("_MainTex");
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        var newPosition = Mathf.Repeat(Time.time*scrollSpeed, tileSizeZ);
+        transform.position = startPosition + Vector3.back*newPosition;
+
+        var x = Mathf.Repeat(Time.time*scrollSpeed, tileSizeZ*4) / tileSizeZ;
+        x = Mathf.Floor(x) / 4;
+
+        var offset = new Vector2(x, _savedOffset.y);
+
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+    void OnDisable()
+    {
+        // Reset the offset to the startup value.
+        GetComponent<Renderer>().sharedMaterial.SetTextureOffset("_MainTex", _savedOffset);
+    }
+}
